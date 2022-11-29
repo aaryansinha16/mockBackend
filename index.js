dd267b9e178c29a4b6da8381a957866678c2be7a
aaryansinha16 <aaryansinha16@gmail.com>
2022-11-29 10:43:43 +0530

    backtobasics

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// server.js
 const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-server.listen(port, () => {
-  console.log('JSON Server is running')
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
